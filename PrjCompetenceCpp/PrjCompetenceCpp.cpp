2db45f1859c25b35bedf0497ee03a01d28c0cd14
Mattéo Diseurs <70532680+LoKiass@users.noreply.github.com>
2026-03-01 17:48:47 +0100

    [+] Ajout de voiture elctrique pour l'utilisateur

--- PrjCompetenceCpp/PrjCompetenceCpp.cpp
+++ PrjCompetenceCpp/PrjCompetenceCpp.cpp
@@ -1,6 +1,6 @@
 
 #include <iostream>
-#include "Voiture.h"
+#include "VoitureElec.h"
 #include "Camion.h"
 #include <vector>
 
@@ -10,22 +10,24 @@ using namespace std;
 * Déclaration de fonction
 */
 void afficherCamions(vector<Camion*> Camions); // Affichage des voitures et de leurs roues
-void saisirInformationVoiture(Voiture& tempVoiture); // Saisie des informations de la voiture et de ses roues
+void afficherVoituresElec(vector<VoitureElec*> voituresElec); // Affichage des voitures électriques et de leurs roues
+void saisirInformationVoiture(Voiture& tempVoiture, bool isElec); // Saisie des informations de la voiture et de ses roues
 
 int main()
 {
 	vector<Camion*> camions;
+	vector<VoitureElec*> voituresElec;
 	string interString;
 	int interInt;
 	// vector<Voiture *> voitures; // Sera ajouter lors des notions de polymorphisme
 	string wait;
 	do {
 		do {
-			cout << "Voulez vous ajouter une voiture ou un camion ? (V/C) : ";
+			cout << "Voulez vous ajouter une voiture electrique ou un camion ? (V/C) : ";
 			cin >> interString;
 			if (interString == "c" || interString == "C") {
 				Camion* tempCamion = new Camion();
-				saisirInformationVoiture(*tempCamion);
+				saisirInformationVoiture(*tempCamion, false);
 
 				do {
 					cout << "Capacité de charge ";
@@ -43,6 +45,24 @@ int main()
 				tempCamion = nullptr;  // Nettoyage mémoire temporaire
 				break;
 			}
+			else if (interString == "v" || interString == "V") {
+				VoitureElec* tempVoitureElec = new VoitureElec();
+				saisirInformationVoiture(*tempVoitureElec, true);
+				do {
+					cout << "Autonomie : ";
+					cin >> interInt;
+					if (!(tempVoitureElec->verifierEntree(interInt))) {
+						cout << "Veuillez entrer une valeur supérieure à 0 pour l'autonomie de la voiture électrique." << endl;
+					}
+					else {
+						tempVoitureElec->setAutonomie(interInt);
+						break;
+					}
+				} while (true); // Verifier si l'autonomie de la voiture électrique est supérieur à 0),
+				voituresElec.push_back(tempVoitureElec);
+				tempVoitureElec = nullptr; // Nettoyage mémoire temporaire
+				break;
+			}
 			else {
 				cout << "Veuillez entrer V pour voiture ou C pour camion." << endl;
 				break;
@@ -58,16 +78,49 @@ int main()
 	} while (true);
 
 	system("cls"); // Nettoyage de la console
-	afficherCamions(camions); // Affichage des voitures et de leurs roues
-
-	for (int x = 0; x < camions.size(); x++) {
-		delete camions[x]; // Libération de la mémoire allouée pour les voitures
+	
+	if (camions.size() > 0) {
+		afficherCamions(camions); // Affichage des voitures et de leurs roues
+		for (int x = 0; x < camions.size(); x++) {
+			delete camions[x]; // Libération de la mémoire allouée pour les voitures
+		}
+	}
+	if (voituresElec.size() > 0) {
+		afficherVoituresElec(voituresElec);
+		for (int x = 0; x < voituresElec.size(); x++) {
+			delete voituresElec[x]; // Libération de la mémoire allouée pour les voitures électriques
+		}
 	}
 	camions.clear(); // Nettoyage du vecteur de voitures
-
 	cin >> wait;
 
 	return 0;
+}
+/*
+* Cette fonction permet d'afficher toute les voitures électriques et leurs roues, ainsi que les informations de leurs moteurs.
+*/
+void afficherVoituresElec(vector<VoitureElec*> voituresElec) {
+	for (int x = 0; x < voituresElec.size(); x++) {
+		cout << "Voiture electrique numero " << x + 1 << " : " << endl;
+		cout << "\tNumero de serie : " << voituresElec[x]->getNumero_serie() << endl;
+		cout << "\tMarque : " << voituresElec[x]->getMarque() << endl;
+		cout << "Moteur : " << endl;
+		cout << "\tNumero de serie : " << voituresElec[x]->moteur.getNumero_serie() << endl;
+		cout << "\tModel : " << voituresElec[x]->moteur.getModel() << endl;
+		cout << "\tPuissance : " << voituresElec[x]->moteur.puissance << endl;
+		for (int y = 0; y < voituresElec[x]->roues.size(); y++) {
+			cout << "Roues numero " << y + 1 << " : " << endl;
+			cout << "\tNumero de serie : " << voituresElec[x]->roues[y].getNumeruo_serie() << endl;
+			cout << "\tMarque : " << voituresElec[x]->roues[y].getMarque() << endl;
+			cout << "\tLargeur : " << voituresElec[x]->roues[y].largeur << endl;
+			cout << "\tHauteur : " << voituresElec[x]->roues[y].hauteur << endl;
+			cout << "\tRadial : " << voituresElec[x]->roues[y].radial << endl;
+			cout << "\tDiametre : " << voituresElec[x]->roues[y].diametre << endl;
+			cout << "\tIndice de charge : " << voituresElec[x]->roues[y].indiceCharge << endl;
+			cout << "\tIndice de vitesse : " << voituresElec[x]->roues[y].indiceVitesse << endl;
+		}
+		cout << "\tAutonomie : " << voituresElec[x]->getAutonomie() <<" km"  << endl;
+	}
 }
 /*
 * Cette fonction permet d'afficher les informations des voitures et de leurs roues.
@@ -100,7 +153,7 @@ void afficherCamions(vector<Camion*> camions) {
 /*
 * Cette fonction permet de saisir les informations d'une voiture et de ses roues.
 */
-void saisirInformationVoiture(Voiture& tempVoiture) {
+void saisirInformationVoiture(Voiture& tempVoiture, bool isElec) {
 	int interInt;
 	string interString;
 	do {
@@ -135,21 +188,22 @@ void saisirInformationVoiture(Voiture& tempVoiture) {
 
 	} while (true); // Verifier si la puissance du moteur est supérieur à 0
 
-	do {
-		cout << "Cylindre : ";
-		cin >> interInt;
-		if (!(tempVoiture.verifierEntree(interInt))) {
-			cout << "Veuillez entrer une valeur supérieure à 0 pour la puissance du moteur." << endl;
-		}
-		else {
-			tempVoiture.moteur.cylindree = interInt;
-			break;
-		}
+	if (isElec) tempVoiture.moteur.cylindree = NULL; // Si la voiture est électrique, la cylindrée du moteur est de 0
+	else {
+		do {
+			cout << "Cylindre : ";
+			cin >> interInt;
+			if (!(tempVoiture.verifierEntree(interInt))) {
+				cout << "Veuillez entrer une valeur supérieure à 0 pour la puissance du moteur." << endl;
+			}
+			else {
+				tempVoiture.moteur.cylindree = interInt;
+				break;
+			}
 
-	} while (true); // Verifier si la cylindrée du moteur est supérieur à 0
-
-
-
+		} while (true); // Verifier si la cylindrée du moteur est supérieur à 0
+	}
+	
 	// Roues de la voiture
 	for (int x = 0; x < tempVoiture.roues.size(); x++) {
 		cout << "Entrez les informations des roues numero " << x + 1 << " de la voiture : " << endl;
